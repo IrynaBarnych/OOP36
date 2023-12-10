6d799729694e7009a17029d7c4170f1cb5debbcd
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-10 20:51:36 +0200

    додала код

--- main.py
+++ main.py
@@ -1,253 +1,221 @@
-# Завдання 1
-# Створіть програму роботи зі словником.
-# Наприклад, англо-іспанський, французько-німецький або інша мовна пара.
-# Програма має:
-# ■ надавати початкове введення даних для словника;
-# ■ відображати слово та його переклади;
-# ■ дозволяти додавати, змінювати, видаляти переклади слова;
-# ■ дозволяти додавати, змінювати, видаляти слово;
-# ■ відображати топ-10 найпопулярніших слів
-# (визначаємо популярність спираючись на лічильник звернень);
-# ■ відображати топ-10 найнепопулярніших слів (визначаємо непопулярність спираючись на лічильник звернень).
-# Використовуйте дерево для виконання цього завдання.
+# Реалізуйте базу даних зі штрафами податкової інспекції.
+# Ідентифікувати кожну конкретну людину буде персональний ідентифікаційний код. В однієї людини може
+# бути багато штрафів.
+# Реалізуйте:
+# 1. Повний друк бази даних;
+# 2. Друк даних за конкретним кодом;
+# 3. Друк даних за конкретним типом штрафу;
+# 4. Друк даних за конкретним містом;
+# 5. Додавання нової людини з інформацією про неї;
+# 6. Додавання нових штрафів для вже існуючого запису;
+# 7. Видалення штрафу;
+# 8. Заміна інформації про людину та її штрафи.
+# Використайте дерево для реалізації цього завдання.
 
-# Завдання 1
-# Створіть програму роботи зі словником.
-# Наприклад, англо-іспанський, французько-німецький або інша мовна пара.
-# Програма має:
-# ■ надавати початкове введення даних для словника;
-# ■ відображати слово та його переклади;
-# ■ дозволяти додавати, змінювати, видаляти переклади слова;
-# ■ дозволяти додавати, змінювати, видаляти слово;
-# ■ відображати топ-10 найпопулярніших слів
-# (визначаємо популярність спираючись на лічильник звернень);
-# ■ відображати топ-10 найнепопулярніших слів (визначаємо непопулярність спираючись на лічильник звернень).
-# Використовуйте дерево для виконання цього завдання.
+class Person:
+    def __init__(self, person_id, name, city):
+        self.id = person_id
+        self.name = name
+        self.city = city
 
-class TreeNode:
-    def __init__(self, key, translation):
-        self.key = key
-        self.translation = translation
+class Penalty:
+    def __init__(self, description, amount):
+        self.description = description
+        self.amount = amount
+
+class PenaltyNode:
+    def __init__(self, penalty):
+        self.penalty = penalty
         self.left = None
         self.right = None
-        self.counter = 1
 
-class DictionaryTree:
-    def __init__(self, initial_data=None):
-        self.root = None
-        self.popular = []
+class TaxDatabase:
+    def __init__(self):
+        self.people = {}
 
-        if initial_data:
-            for key, translation in initial_data:
-                self.add_word(key, translation)
+    def add_person(self, person):
+        if person.id not in self.people:
+            self.people[person.id] = {"person": person, "penalties": None}
 
-    def add_word(self, key, translation):
-        if not self.root:
-            self.root = TreeNode(key, translation)
-        else:
-            self._add_word(self.root, key, translation)
+    def add_penalty(self, person_id, penalty):
+        if person_id in self.people:
+            new_penalty_node = PenaltyNode(penalty)
+            self.people[person_id]["penalties"] = self._insert_penalty(self.people[person_id]["penalties"], new_penalty_node)
 
-    def _add_word(self, node, key, translation):
-        if key == node.key:
-            node.translation = translation
-            node.counter += 1
-        elif key < node.key:
-            if node.left is None:
-                node.left = TreeNode(key, translation)
+    def display_database(self):
+        for person_id, data in self.people.items():
+            person = data["person"]
+            penalties = data["penalties"]
+            print(f"Ідентифікаційний номер особи: {person.id}, Ім'я: {person.name}")
+            if penalties is not None:
+                self._display_penalties(penalties)
             else:
-                self._add_word(node.left, key, translation)
+                print("Штрафів немає")
+
+    def _display_penalties(self, penalty_node):
+        if penalty_node:
+            self._display_penalties(penalty_node.left)
+            print(f"  Штраф: {penalty_node.penalty.description}, Сума: {penalty_node.penalty.amount}")
+            self._display_penalties(penalty_node.right)
+
+    def _insert_penalty(self, root, new_penalty_node):
+        if not root:
+            return new_penalty_node
+        if new_penalty_node.penalty.amount < root.penalty.amount:
+            root.left = self._insert_penalty(root.left, new_penalty_node)
         else:
-            if node.right is None:
-                node.right = TreeNode(key, translation)
+            root.right = self._insert_penalty(root.right, new_penalty_node)
+        return root
+
+    def remove_penalty(self, root, description):
+        if not root:
+            return root
+
+        if description < root.penalty.description:
+            root.left = self.remove_penalty(root.left, description)
+        elif description > root.penalty.description:
+            root.right = self.remove_penalty(root.right, description)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+            root.penalty = self._min_value_node(root.right)
+            root.right = self.remove_penalty(root.right, root.penalty.description)
+
+        return root
+
+    def _min_value_node(self, node):
+        current = node
+        while current.left:
+            current = current.left
+        return current
+
+    def display_person_data(self, person_id):
+        data = self.people.get(person_id)
+        if data is not None:
+            person = data["person"]
+            penalties = data["penalties"]
+            print(f"Ідентифікаційний номер особи: {person.id}, Ім'я: {person.name}")
+            if penalties is not None:
+                self._display_penalties(penalties)
             else:
-                self._add_word(node.right, key, translation)
-
-    def display_top_popular(self):
-        self.popular = []
-        self._get_popular_words(self.root)
-        if self.popular:
-            print("Топ 10")
-            for word in self.popular[:10]:
-                print(f'{word.key}: {word.counter} було додано разів')
+                print("Штрафів немає")
         else:
-            print("Слова відсутні")
+            print(f"Особи з ідентифікаційним номером {person_id} не існує в базі даних.")
 
-    def _get_popular_words(self, node):
-        if node is not None:
-            self._get_popular_words(node.left)
-            self.popular.append(node)
-            self.popular.sort(key=lambda x: x.counter, reverse=True)
-            if len(self.popular) > 10:
-                self.popular.pop()
-            self._get_popular_words(node.right)
+    def display_penalty_type_data(self, penalty_type):
+        found = False
+        for person_id, data in self.people.items():
+            penalties = data["penalties"]
+            if penalties is not None:
+                self._display_penalties_by_type(penalties, penalty_type, person_id)
+        if not found:
+            print(f"Штрафів за типом '{penalty_type}' не знайдено в базі даних.")
 
-    def display_top_unpopular(self):
-        self.popular = []
-        self._get_unpopular_words(self.root)
-        if self.popular:
-            print("\nТоп-10 найнепопулярніших слів:")
-            for word in reversed(self.popular[-10:]):
-                print(f'{word.key}: {word.counter} було додано разів')
+    def _display_penalties_by_type(self, penalty_node, penalty_type, person_id):
+        if penalty_node:
+            self._display_penalties_by_type(penalty_node.left, penalty_type, person_id)
+            if penalty_node.penalty.description == penalty_type:
+                found = True
+                person = self.people[person_id]["person"]
+                print(f"Ідентифікаційний номер особи: {person.id}, Ім'я: {person.name}")
+                print(f"  Штраф: {penalty_node.penalty.description}, Сума: {penalty_node.penalty.amount}")
+            self._display_penalties_by_type(penalty_node.right, penalty_type, person_id)
+
+    def display_city_data(self, city):
+        found = False
+        for person_id, data in self.people.items():
+            person = data["person"]
+            penalties = data["penalties"]
+            if person.city == city and penalties is not None:
+                found = True
+                print(f"Ідентифікаційний номер особи: {person.id}, Ім'я: {person.name}, Місто: {person.city}")
+                self._display_penalties(penalties)
+
+        if not found:
+            print(f"Штрафів для осіб з містом '{city}' не знайдено в базі даних.")
+
+    def add_new_person_info(self, person_id, name, city):
+        new_person = Person(person_id, name, city)
+        self.add_person(new_person)
+
+    def add_penalty_to_person(self, person_id, penalty):
+        if person_id in self.people:
+            new_penalty_node = PenaltyNode(penalty)
+            self.people[person_id]["penalties"] = self._insert_penalty(self.people[person_id]["penalties"],
+                                                                       new_penalty_node)
+            print(f"Штраф додано до особи з ідентифікаційним номером {person_id}.")
         else:
-            print("Слова відсутні")
+            print(f"Особи з ідентифікаційним номером {person_id} не існує в базі даних. Додавання штрафу не виконано.")
 
-    def _get_unpopular_words(self, node):
-        if node is not None:
-            self._get_unpopular_words(node.left)
-            self.popular.append(node)
-            self.popular.sort(key=lambda x: x.counter)
-            if len(self.popular) > 10:
-                self.popular.pop()
-            self._get_unpopular_words(node.right)
-
-    def display_translations(self, key, is_ukrainian=False):
-        node = self._search(self.root, key, is_ukrainian)
-        if node:
-            print(f'{node.key}: {node.translation}')
-            return node.translation
-        else:
-            print(f'{key} не знайдено в словнику')
-            return None
-
-    def _search(self, node, key, is_ukrainian=False):
-        if node is None or node.key.lower() == key.lower():
-            return node
-        if (is_ukrainian and key.lower() < node.key.lower()) or (not is_ukrainian and key < node.key):
-            return self._search(node.left, key, is_ukrainian)
-        elif (is_ukrainian and key.lower() > node.key.lower()) or (not is_ukrainian and key > node.key):
-            return self._search(node.right, key, is_ukrainian)
-
-    def update_translation(self, key, old_translation, new_translation):
-        node = self._search(self.root, key)
-        if node and node.translation == old_translation:
-            node.translation = new_translation
-            print(f'Переклад для {key} оновлено: {new_translation}')
-        elif node:
-            print(f'{old_translation} не є поточним перекладом для {key}')
-        else:
-            print(f'{key} не знайдено в словнику')
-
-    def delete_translations(self, key):
-        node = self._search(self.root, key)
-        if node:
-            confirm = input(f'Ви впевнені, що хочете видалити перший варіант перекладу для {key}? (Так або ні): ')
-            if confirm.lower() == "так":
-                node.left = None
-                node.right = None
-                print(f'Початковий переклад для {key} видалено')
-        else:
-            print(f'{key} не знайдено в словнику')
-
-
-    def add_translation(self, key, new_translation):
-        node = self._search(self.root, key)
-        if node:
-            self._add_word(node, key, new_translation)
-            print(f'Переклад для {key} додано: {new_translation}')
-        else:
-            self.add_word(key, new_translation)
-            print(f'{key}: {new_translation} додано до словника')
-
-    def add_word_with_translation(self, key, translation):
-        node = self._search(self.root, key)
-        if node:
-            print(f'{key} вже присутнє в словнику')
-        else:
-            self.add_word(key, translation)
-            print(f'{key}: {translation} додано до словника')
-
-    def add_word_english(self, key, translation):
-        node = self._search(self.root, key)
-        if node:
-            print(f'{key} вже присутнє в словнику')
-        else:
-            self.add_word(key, translation)
-            print(f'{key}: {translation} додано до словника')
-
-    def print_menu(self):
-        print("\nМеню:")
-        print("1. Надати початкове введення даних для словника")
-        print("2. Відобразити слово та його переклади")
-        print("3. Додати, змінити, видалити переклади слова (англійська мова)")
-        print("4. Додати, змінити, видалити переклади слова (українська мова)")
-        print("5. Відобразити топ-10 найпопулярніших слів")
-        print("6. Відобразити топ-10 найнепопулярніших слів")
-        print("0. Вихід")
-
-    def main(self):
-        initial_data = [
-            ("apple", "яблуко"),
-            ("apple", "яблучко"),
-            ("banana", "банан"),
-            ("kiwi", "ківі"),
-            ("apricot", "абрикос"),
-            ("cherry", "вишня"),
-            ("plum", "слива"),
-            ("grape", "виноград"),
-            ("pear", "груша"),
-            ("apricot", "алича"),
-            ("strawberry", "полуниця"),
-            ("raspberry", "малина"),
-            ("яблуко", "apple"),
-            ("яблучко", "apple"),
-            ("банан", "banana"),
-            ("ківі", "kiwi"),
-            ("абрикос", "apricot"),
-            ("вишня", "cherry"),
-            ("слива", "plum"),
-            ("виноград", "grape"),
-            ("груша", "pear"),
-            ("алича", "apricot"),
-            ("полуниця", "strawberry"),
-            ("малина", "raspberry")
-        ]
-
-        dictionary = DictionaryTree(initial_data)
-
-        while True:
-            self.print_menu()
-            choice = input("Оберіть опцію (введіть число): ")
-
-            if choice == "0":
-                print("Дякую за використання програми!")
-                break
-            elif choice == "1":
-                key = input("Введіть слово: ")
-                new_translation = input("Введіть переклад: ")
-                dictionary.add_word(key, new_translation)
-            elif choice == "2":
-                key = input("Введіть слово: ")
-                displayed_translation = dictionary.display_translations(key)
-            elif choice == "3":
-                key = input("Введіть слово (англ. мова): ")
-                displayed_translation = dictionary.display_translations(key)
-                if displayed_translation is not None:
-                    translation = input("Введіть переклад слова: ")
-                    new_translation = input("Введіть новий переклад: ")
-                    dictionary.update_translation(key, displayed_translation, new_translation)
-
-                    confirm = input("Чи бажаєте видалити початковий переклад слова? (Так або ні): ")
-                    if confirm.lower() == "так":
-                        dictionary.delete_translations(key)
-            elif choice == "4":
-                key = input("Введіть слово (укр. мова): ")
-                displayed_translation = dictionary.display_translations(key)
-                if displayed_translation is not None:
-                    translation = input("Введіть переклад слова: ")
-                    new_translation = input("Введіть новий переклад: ")
-                    dictionary.update_translation(key, displayed_translation, new_translation)
-
-                    confirm = input("Чи бажаєте видалити переклади слова? (Так або ні): ")
-                    if confirm.lower() == "так":
-                        dictionary.delete_translations(key)
-
-            elif choice == "5":
-                dictionary.display_top_popular()
-            elif choice == "6":
-                dictionary.display_top_unpopular()
+    def remove_penalty_from_person(self, person_id, penalty_description):
+        if person_id in self.people:
+            penalties = self.people[person_id]["penalties"]
+            if penalties is not None:
+                self.people[person_id]["penalties"] = self.remove_penalty(penalties, penalty_description)
             else:
-                print("Невірний вибір. Спробуйте ще раз.")
+                print(f"Особа з ідентифікаційним номером {person_id} не має жодного штрафу. Видалення не виконано.")
+        else:
+            print(f"Особи з ідентифікаційним номером {person_id} не існує в базі даних. Видалення не виконано.")
 
-if __name__ == "__main__":
-    dictionary_instance = DictionaryTree()
-    dictionary_instance.main()
+    def update_person_info(self, person_id, new_name, new_city):
+        if person_id in self.people:
+            self.people[person_id]["person"].name = new_name
+            self.people[person_id]["person"].city = new_city
+            print(f"Інформацію про особу з ідентифікаційним номером {person_id} змінено.")
+
+
+tax_database = TaxDatabase()
+
+person1 = Person(1, "Тарас Шевченко", "Київ")
+person2 = Person(2, "Іван Франко", "Суми")
+
+new_person_id = 3
+new_person_name = "Леся Українка"
+new_person_city = "Львів"
+
+tax_database.add_person(person1)
+tax_database.add_person(person2)
+
+penalty1 = Penalty("Запізнене подання податкових документів", 100)
+penalty2 = Penalty("Не заплачено", 50)
+
+tax_database.add_penalty(1, penalty1)
+tax_database.add_penalty(1, penalty2)
+tax_database.add_penalty(2, penalty1)
+
+tax_database.add_new_person_info(new_person_id, new_person_name, new_person_city)
+
+print("\nВідображення податкової бази даних:")
+tax_database.display_database()
+
+print("\nВідображення даних за конкретним ідентифікаційним номером:")
+tax_database.display_person_data(1)
+
+print("\nВідображення даних за конкретним типом штрафу:")
+tax_database.display_penalty_type_data("Запізнене подання податкових документів")
+
+print("\nВідображення даних за конкретним містом:")
+tax_database.display_city_data("Київ")
+
+print("\n Відображення податкової бази даних після додавання нової людини:")
+tax_database.display_database()
+
+new_penalty3 = Penalty("Несплачено податок на нерухомість", 200)
+tax_database.add_penalty_to_person(1, new_penalty3)
+
+print("\n Відображення податкової бази даних після додавання нового штрафу:")
+tax_database.display_database()
+
+
+tax_database.remove_penalty_from_person(1, "Запізнене подання податкових документів")
+
+print("\n Відображення податкової бази даних після видалення штрафу:")
+tax_database.display_database()
+
+tax_database.update_person_info(1, "Нове Ім'я", "Нове Місто")
+
+print("\n Відображення податкової бази даних після зміни інформації про особу:")
+tax_database.display_database()
